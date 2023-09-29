43418a0966fa251ab0aad2d466fe56b7d32c477e
John Elliott <j.elliott1700@gmail.com>
2023-09-29 10:31:27 -0700

    added footer comment

--- src/App.js
+++ src/App.js
@@ -9,20 +9,18 @@ import {
   Nav,
   NavItem,
 } from "reactstrap";
-import { Col, Container, Row } from "reactstrap";
 import { useSpring, animated } from "react-spring";
 import ContactForm from "./components/ContactForm"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyLogo from "./images/logo.png";
 
-import githubIcon from "./images/008-github.png";
-import linkedinIcon from "./images/010-linkedin.png";
 import ScrollToTop from "./utils/ScrollToTop";
 import About from "./features/About";
 import Profesional from "./components/Profesional";
 import Skills from "./features/Skills";
 import Education from "./features/Education";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -148,46 +146,9 @@ function App() {
         </animated.div>
 
         {/* Footer */}
-        <footer className="site-footer">
-          <Container>
-            <Row>
-              <Col xs="6" sm="3" className="text-center">
-                <h5>Social</h5>
-                <a
-                  className="btn btn-social-icon btn-instagram social-icon"
-                  href="http://instagram.com/"
-                >
-                  <i className="fa fa-instagram" />
-                </a>{" "}
-                <a
-                  className="btn btn-social-icon btn-facebook social-icon"
-                  href="http://www.facebook.com/"
-                >
-                  <i className="fa fa-facebook" />
-                </a>{" "}
-                <a
-                  className="btn btn-social-icon btn-twitter social-icon"
-                  href="http://twitter.com/"
-                >
-                  <i className="fa fa-twitter" />
-                </a>{" "}
-              </Col>
-              <Col>
-                <a href="https://github.com/NurdPlay">
-                  <img src={githubIcon} alt="GitHub" className="social-icon" />
-                </a>
-                <a href="https://www.linkedin.com/in/john-elliott-0a13a31b1/">
-                  <img
-                    src={linkedinIcon}
-                    alt="LinkedIn"
-                    className="social-icon"
-                  />
-                </a>
-              </Col>
-            </Row>
-          </Container>
-        </footer>
-      </div>
+          <Footer />
+
+              </div>
     </Router>
   );
 }
