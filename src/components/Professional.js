81f34166bf825d34c7e3755a370040ba91008295
John Elliott <j.elliott1700@gmail.com>
2023-10-02 14:29:22 -0700

    formated code

--- src/components/Professional.js
+++ src/components/Professional.js
@@ -6,33 +6,30 @@ function Profesional() {
   return (
     <Container className="professional-container">
       <Row>
-      <h3 className="professional-title">Professional Links</h3>
+        <h3 className="professional-title">Professional Links</h3>
       </Row>
-      
-        
+
       <div className="professional-links">
-      <a
-    href="https://github.com/NurdPlay"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="pro-link"
-  >
-    <img src={githubIcon} alt="GitHub" className="social-icon-about" />
-  </a>
-  <a
-    href="https://www.linkedin.com/in/john-elliott-0a13a31b1/"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <img
-      src={linkedinIcon}
-      alt="LinkedIn"
-      className="social-icon-about"
-    />
-  </a>
-      
+        <a
+          href="https://github.com/NurdPlay"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="pro-link"
+        >
+          <img src={githubIcon} alt="GitHub" className="social-icon-about" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/john-elliott-0a13a31b1/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={linkedinIcon}
+            alt="LinkedIn"
+            className="social-icon-about"
+          />
+        </a>
       </div>
-        
     </Container>
   );
 }
